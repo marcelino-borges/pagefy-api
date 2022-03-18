6b57e7ee35d7e2bf45054a1d4a00fc12204d3e4c
Marcelino Borges <botelho_gt@hotmail.com>
2022-03-18 20:06:39 -0300

    removed token verification from increment component clicks route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -40,11 +40,7 @@ router.get(
   pageController.getAllUserPagesByUserId
 );
 router.post("/page", verifyToken, pageController.createUserPage);
-router.post(
-  "/page/component-clicks",
-  verifyToken,
-  pageController.incrementComponentClicks
-);
+router.post("/page/component-clicks", pageController.incrementComponentClicks);
 router.put("/page", verifyToken, pageController.updateUserPage);
 router.delete("/page/id/:pageId", verifyToken, pageController.deleteUserPage);
 
