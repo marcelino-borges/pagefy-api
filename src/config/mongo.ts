f8ea991c212d74e56ab8ba7610ab72c5e2042103
Marcelino Borges <botelho_gt@hotmail.com>
2022-09-17 00:05:23 +0100

    start script using ts-node

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -5,6 +5,10 @@ const connect = async () => {
 
   console.log("Connecting to mongo...");
 
+  console.log(
+    "process.env.MONGO_CONNECTION_STRING:",
+    process.env.MONGO_CONNECTION_STRING
+  );
   await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
